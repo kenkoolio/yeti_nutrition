add1ab9769ed273175cb63c873679945f2e7b3a9
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2020-04-08 22:40:51 -0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*********************************************************************
+** Program name: First Push
+** Author: Sariah Bunnell 
+** Date:  April 8 2020
+** Description: This program is meaningless and is simply to 
+** practice a push and a commit 
+*********************************************************************/
+#include <iostream>
+
+
+int main()
+{
+  std::cout << "This is a test! " << std::endl;
+  return 0;
+}
