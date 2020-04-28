882622076ec2cdb73e3deb48f86a268fd6f34990
Ken Nguyen <aznboikenny@yahoo.com>
2020-04-27 23:53:05 -0500

    added pipeline to add new ingredient

--- routes/admin.js
+++ routes/admin.js
@@ -8,13 +8,41 @@ module.exports = (function() {
 
   function addNewIngredient(req, res, next) {
     let ingredient_name = req.body.ingredient_name.toLowerCase();
-    let query = `INSERT INTO ingredients (ingredient_name) VALUES (?)`;
     let mysql = req.app.get('mysql');
+    let check_query = `SELECT * FROM ingredients WHERE ingredient_name = ?`;
+    let insert_query = `INSERT INTO ingredients (ingredient_name) VALUES (?)`;
 
-    mysql.pool.query(query, ingredient_name, (err, results, fields) => {
-      if (err) return next(err);
+    new Promise((resolve, reject) => {
+      // check if ingredient already in database
+      mysql.pool.query(check_query, ingredient_name, (err, results, fields) => {
+        if (err) return next(err);
 
-      return res.redirect('/admin');
+        if (results.length) {
+          // already in database
+          reject("That item is already in the database");
+        } else {
+          // not in database yet
+          resolve();
+        }
+      })
+    })
+    .then(() => {
+      // insert new ingredient
+      return new Promise((resolve, reject) => {
+        mysql.pool.query(insert_query, ingredient_name, (err, results, fields) => {
+          if (err) return next(err);
+
+          resolve();
+        })
+      })
+    })
+    .then(() => {
+      // return success
+      return res.json('Success');
+    })
+    .catch((reason) => {
+      // return error message
+      return res.status(400).json(reason);
     })
   }
 
