9c9227feaca1f37eadc39bc1bef8db81757e0940
vanruisg <59535537+vanruisg@users.noreply.github.com>
2020-05-08 20:10:27 -0500

    Working message board (no flash yet)

--- dbcon.js
+++ dbcon.js
@@ -1,34 +1,9 @@
-// dbcon.js
-// Created: 4/12/2020
-// Author: Huy Nguyen <nguyehu6@oregonstate.edu>
-// Description: File to set up node connection to mysql database.
-
-const dotenv = require('dotenv');
 var mysql = require('mysql');
-
-dotenv.config();
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : process.env.DBHOST,
-  user            : process.env.USERNAME,
-  password        : process.env.PASSWORD,
-  database        : process.env.DB_NAME
+    connectionLimit: 10,
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_vanruisg',
+    password: '1814',
+    database: 'cs340_vanruisg'
 });
-
 module.exports.pool = pool;
-
-
-// Instructions for Local DB connection setup
-
-// Create a file called “.env” in the project root directory
-// Insert these fields with the credentials of your local mysql username and password,
-//   host should be blank, and db name should be “com_unity”:
-
-// # Database credentials
-// USERNAME="root"
-// PASSWORD="your_password"
-// HOST=""
-// DB_NAME="yeti_nutrition"
-//
-// # other local environment stuff
-// PORT=8000
