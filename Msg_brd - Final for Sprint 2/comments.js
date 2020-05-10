67ed8493a933ccfd6138571fdef0bcc147a4f7c7
vanruisg <59535537+vanruisg@users.noreply.github.com>
2020-05-09 21:27:34 -0500

    Updated files
    Fixed all issues w/ return statements, etc. Removed dbcon.js for this commit.

---
+++ Msg_brd - Final for Sprint 2/comments.js
@@ -0,0 +1,83 @@
+module.exports = function () {
+
+    var express = require('express');
+    var router = express.Router();
+    
+
+    // Retrieve all comments
+    function getComments(res, mysql, context, complete) {
+        mysql.pool.query("SELECT c.comment_id, c.comment_date, u.user_name, p.post_id, c.content FROM comments c INNER JOIN users u ON u.user_id = c.user_id", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+                return;
+            }
+            context.comments = results;
+            complete();
+        });
+    }
+
+    // Retrieve comments for a single parent post
+    function getCommentsByPost(req, res, mysql, context, complete) {
+        var query = "SELECT c.comment_id, c.comment_date, u.user_name, p.post_id, c.content FROM comments c INNER JOIN posts p ON c.post_id = p.post_id INNER JOIN users u ON u.user_id = c.user_id WHERE p.post_id = ?";
+        console.log(req.params);
+        var inserts = [req.params.postID];
+        mysql.pool.query(query, inserts, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+                return;
+            }
+            context.comments = results;
+            complete();
+        });
+    }
+
+    // Display all comments for all posts
+    router.get('/', function (req, res) {
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getComments(res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('comments', context);
+            }
+        }
+    });
+
+    // Display all comments for a single parent post
+    router.get('/comments/:postID', function (req, res) {
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getCommentsByPost(req, res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('comments', context);
+            }
+        }
+    });
+
+    
+
+    router.post('/create_comment', function (req, res) {
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO comments (post_id, user_id, content, comment_date) VALUES (?,?,?,?)";
+        var date = new Date();
+        var inserts = [req.body.post_id, req.body.user_id, req.body.content, date];
+        sql = mysql.pool.query(sql, inserts, function (error, results, field) {
+            if (error) {
+                console.log(JSON.stringify(error));
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                res.redirect('../posts/' + req.body.post_id);
+            }
+        });
+    });
+
+    return router;
+}();
---
+++ Msg_brd - Final for Sprint 2/posts.js
@@ -0,0 +1,124 @@
+module.exports = function () {
+
+    var express = require('express');
+    var router = express.Router();
+
+    // Retrieve all posts
+    function getPosts(res, mysql, context, complete) {
+        console.log('Retrieving all posts');
+        mysql.pool.query("SELECT p.post_id, p.post_date, u.user_name, p.title, p.content FROM posts p INNER JOIN users u ON u.user_id = p.user_id ORDER BY p.post_date ASC", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+                return;
+            }
+            context.posts = results;
+            complete();
+        });
+    }
+
+    // Retrieve a single post
+    function getSinglePost(res, mysql, context, id, complete) {
+        console.log('Retrieving a single post');
+        var sql = "SELECT p.post_id, p.post_date, u.user_name, p.title, p.content FROM posts p INNER JOIN users u ON u.user_id = p.user_id WHERE post_id = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+                return;
+            }
+            context.post = results[0];
+            complete();
+        });
+    }
+
+    // Retrieve comments for a single parent post
+    function getCommentsByPost(res, mysql, context, post_id, complete) {
+        console.log('Retrieving all comments for post:' + post_id);
+        var query = "SELECT c.comment_id, c.comment_date, u.user_name, p.post_id, c.content FROM comments c INNER JOIN posts p ON c.post_id = p.post_id INNER JOIN users u ON u.user_id = c.user_id WHERE p.post_id = ? ORDER BY c.comment_date ASC";
+        var inserts = [post_id];
+        mysql.pool.query(query, inserts, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+                return;
+            }
+            context.comments = results;
+            complete();
+        });
+    }
+
+    // Display all message board posts
+    router.get('/', function (req, res) {
+        console.log('Accessing the main page');
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getPosts(res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('posts', context);
+            }
+        }
+    });
+
+    // Display homepage with flash message
+    router.get('/post_submitted', function (req, res) {
+        console.log('Accessing the main page');
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getPosts(res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('first_view', context);
+            }
+        }
+    });
+
+    // Display form to create a new post
+    router.get('/create_post_form', function (req, res) {
+        console.log('This should be loading the form');
+        res.render('create_post_form');
+    });
+
+    // Display a single message board post
+    router.get('/:id', function (req, res) {
+        console.log('Accessing a single post page');
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getSinglePost(res, mysql, context, req.params.id, complete);
+        getCommentsByPost(res, mysql, context, req.params.id, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 2) {
+                res.render('post', context);
+            }
+        }
+    });
+
+    // Create a new message board post
+    router.post('/create_post', function (req, res) {
+        console.log('Inserting into the posts table');
+        console.log(req.body);
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO posts (user_id, title, content, post_date) VALUES (?,?,?,?)";
+        var date = new Date();
+        var inserts = [req.body.user_id, req.body.title, req.body.content, date];
+        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                console.log(JSON.stringify(error));
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                res.redirect('/posts/post_submitted');
+            }
+        });
+    });
+
+    return router;
+}();
